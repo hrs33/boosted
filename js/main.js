2be22a77d7d68ce6a1abb0cd810d65ffd9ad6737
hraheja33 <89431410+hraheja33@users.noreply.github.com>
2021-09-15 14:46:29 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.querySelector(".flex-column").lastElementChild.firstElementChild.href = "contact.html";
+
+document.querySelector(".navbar-nav").children[3].firstElementChild.href = "contact.html";
